6988d69f3b23b5c208671cf400894662b410d419
ksnapes <ksnapes@live.co.uk>
2015-03-28 21:51:33 +1300

    Update TVSorter.py
    Added menu options and added better flow. Added extra

--- TVSorter.py
+++ TVSorter.py
@@ -1,63 +1,74 @@
 '''
-Created on Mar 10, 2015
 
+Created on Mar 10, 2015
 @author: Snapes
+
 '''
 import os
 
 endSession = False
+ 
 
-while (endSession == False):
-      
-    currentDir = "C:\\Users\\Kieran\\Files\\Torrents\\Bobs Burgers" 
-    itemList = []
-    fileName = input("Enter file name to sort: ").lower()
-    sortBy = fileName.split()
-    folderName = input("Create folder with name: ")
+def displayOptions(currentDir):
+    for file in os.listdir(path=currentDir):
+        print(file)
+     
+def sortFiles(currentDir):
     
-    def loadFiles():
-        for file in os.listdir(path=currentDir):
-            if (file != folderName):
-                itemList.append(file)
-            
+    tagMatch = []
+    tag = input("Enter tag to sort by: ")
+    sortToFolder = input("Sort to folder: ")
+    sortByTags = tag.split()
     
-    def sortFiles(name):
-        matching = []
-        for file in itemList:
-            termCount = 0
-            for term in sortBy:
+    for file in os.listdir(path=currentDir):
+        if (file != sortToFolder):
                 
+            termCount = 0
+            for term in sortByTags:
+    
                 if term in file.lower():
                     termCount = termCount + 1
-                
-            if(termCount == len(sortBy)):
-                print(file + "\n")
-                matching.append(file)
-                
-        confirm = input("Do you want to move these files? [Y/N]: ")
-        
-        if (confirm.lower() == "n"):
-            print("Move cancelled")
-            return
-        elif (confirm.lower() == "y"):
-            print("Moving...")
-                
-        if not os.path.exists("C:\\Users\\Kieran\\Files\\Torrents\\Bobs Burgers\\" + folderName):
-            os.makedirs("C:\\Users\\Kieran\\Files\\Torrents\\Bobs Burgers\\" + folderName)
-        
-        for file in matching:
-            os.rename("C:\\Users\\Kieran\\Files\\Torrents\\Bobs Burgers\\" + file, "C:\\Users\\Kieran\\Files\\Torrents\\Bobs Burgers\\" + folderName +"\\" + file)
-            
-        #os.close(0)
-        if(confirm.lower() == "y"):
-            print("Move complete.")
-            
-        
-        
-    loadFiles()
-    sortFiles(fileName)
     
-    userChoice = input("Exit Show Sorter? [Y/N]").lower()
-    if(userChoice == "y"):
-        endSession = True
+            if(termCount == len(sortByTags)):
+                print(file + "\n")
+                tagMatch.append(file)
+                
+    confirm = input("Do you want to move these files? [Y/N]: ")
         
+    if (confirm.lower() == "n"):
+        print("Move cancelled")
+        return
+    elif (confirm.lower() == "y"):
+        print("Moving...")
+                
+    if not os.path.exists(currentDir + sortToFolder):
+        os.makedirs(currentDir + sortToFolder)
+        
+    for file in tagMatch:
+        os.rename(currentDir + file, currentDir + sortToFolder + "\\" + file)
+            
+
+    if(confirm.lower() == "y"):
+        print("Move complete.")
+
+
+currentDir = input("Enter directory to be sorted: ")
+if not currentDir.endswith("\\"):
+    currentDir = currentDir + "\\"
+    
+while(endSession == False):
+    print("\n1. Display files")
+    print("2. Sort files")
+    print("3. Change directory")
+    print("4. Quit \n")
+    userChoice = input("Type number to select option: ")
+    if(userChoice == "1"):
+        displayOptions(currentDir)
+    elif(userChoice == "2"):
+        sortFiles(currentDir)
+    elif(userChoice == "3"):
+        currentDir = input("Enter new directory: ")
+    elif(userChoice == "4"):
+        endSession = True
+    
+    
